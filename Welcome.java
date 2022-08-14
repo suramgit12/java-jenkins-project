d3e4de25bf762f3e324ca979a9b5ff553a23cbaa
suramgit <sura2india@gmail.com>
2022-08-14 22:12:09 +0530

    Third release of  Welcome Java!

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,7 @@
 public class Welcome{
 	public static void main(String []arg){
 		System.out.println("Welcome to Java in Jenkins pipeline");
-		System.out.println("This is 2nd line to add");
+		System.out.println("This is 2nd line to add and check");
+		
 	}
 }
